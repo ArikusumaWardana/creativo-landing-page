7f4b3d2a4a362049686ca828847159cdb08136fb
ArikusumaWardana <arikusumawardana09@gmail.com>
2025-04-13 23:54:28 +0800

    Refactor Button component to use TypeScript interface for props definition

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,4 +1,10 @@
-export default function Button({ text, style, onClick }: { text: string, style: string, onClick?: () => void }) {
+interface ButtonProps {
+  text: string;
+  style: string;
+  onClick?: () => void;
+}
+
+export default function Button({ text, style, onClick }: ButtonProps) {
   return (
     <button className={`rounded-lg transition-colors font-inter text-sm font-bold uppercase ${style}`} onClick={onClick}>
       {text}
