4db7ce64b1b2a83da4e7523201d9f2365a0cb390
ArikusumaWardana <arikusumawardana09@gmail.com>
2025-04-14 23:00:18 +0800

    Add AOS initialization in DefaultLayout

--- src/app/components/templates/DefaultLayout.tsx
+++ src/app/components/templates/DefaultLayout.tsx
@@ -1,12 +1,24 @@
+'use client'
+
 import { ReactNode } from 'react';
 import Navbar from '../organisms/Navbar';
 import Footer from '../organisms/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 interface DefaultLayoutProps {
   children: ReactNode;
 }
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
+  useEffect(() => {
+    AOS.init({
+      easing: 'ease-out-quad',
+      duration: 1000,
+    });
+  }, []);
+  
   return (
     <div className="min-h-screen">
       <Navbar />
